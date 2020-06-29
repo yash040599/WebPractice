1223fd6ebbc5c1702bc2bfdd3732f95097e7d32a
Yash <yash040599@gmail.com>
2020-06-29 16:45:02 +0530

    fsfew

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -2,8 +2,5 @@
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
-    $scope.myCol = 'Violet';
-
-    var hobbies = ['Playing & Gaming', 'Watching movies in theater', 'Coding, coz money!']
-    $scope.hobbies = hobbies;
+    
 });
